54dbc78e005e15391baa27553ff5ebdc30a07a02
ALKESH\Alkesh <akleshsakhare@yahoo.com>
2021-08-30 20:17:16 +0530

    first commit

---
+++ src/test/java/testcases/SimpleTest.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+	@Test
+	private void method() {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
+}
